cdaa70d4f6e0181aa79ef9ac1017160bd090e0d6
DaxH <52769764+DaxH@users.noreply.github.com>
2020-02-16 23:22:36 -0500

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,46 @@
+$(function () {
+  // chat
+  let socket = io();
+  let mensaje = $('#mensaje-chat');
+  let chat = $('#chat');
+
+  // usuario
+  let $usuario_form = $('#usuario-form');
+  let $usuario_nombre = $('#usuario-nombre');
+  let $usuario_error = $('#usuario-error');
+  let $usuarios_activos = $('#usuario-activo');
+
+  $usuario_form.submit(function(evento) {
+    evento.preventDefault();
+    socket.emit('nuevo-usuario', $usuario_nombre.val(), function(data) {
+      if (data) {
+        $('#contenedor-usuario').hide();
+        $('#contenedor-chat').show();
+      }else {
+        $usuario_error.html(`<div class="alert alert-danger">Ese Usuario ya Existe</div>`);
+      }
+      $usuario_nombre.val('');
+    });
+  });
+
+  socket.on('usuarios-conectados', function(data){
+    let html = '';
+    for (var i = 0; i < data.length; i++) {
+      html += `<p>${data[i]}</p>`
+    }
+    $usuarios_activos.html(html);
+  });
+
+  mensaje.focus();
+
+  $('#caja-mensaje').submit(function(evento) {
+    evento.preventDefault();
+
+    socket.emit('mensaje-del-cliente', mensaje.val());
+    mensaje.val('');
+  });
+
+  socket.on('mensaje-del-servidor', function (data) {
+    chat.append('<b>' + data.usuario + '</b>:' + data.mensaje + '<br/>')
+  });
+});
---
+++ sockets.js
@@ -0,0 +1,42 @@
+const io = require('socket.io');
+module.exports = function (server) {
+
+  let sockets = io.listen(server);
+  let usuario_list = [];
+
+  sockets.on('connection',function(socket) {
+    console.log('nuevo cliente conectado');
+
+    socket.on('nuevo-usuario', function(data, respuesta){
+      console.log(data);
+      if (usuario_list.indexOf(data) != -1) {
+        respuesta(false);
+      }else {
+
+        respuesta(true);
+        socket.usuario = data;
+        usuario_list.push(socket.usuario);
+        sockets.emit('usuarios-conectados', usuario_list)
+
+      }
+    });
+
+    socket.on('mensaje-del-cliente', function (data) {
+        sockets.emit('mensaje-del-servidor',{
+          mensaje:data,
+          usuario: socket.usuario
+        })
+
+    });
+
+    socket.on('disconnect', function(data)  {
+
+      if (!socket.usuario) return;
+
+      usuario_list.splice(usuario_list.indexOf(socket.usuario))
+      sockets.emit('usuarios-conectados', usuario_list)
+
+    });
+
+  });
+}
